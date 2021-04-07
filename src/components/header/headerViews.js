f33f6e318ba052e002b4fd3779bd289684c2ee74
CelerantDevteam <81623214+CelerantDevteam@users.noreply.github.com>
2021-04-07 06:30:12 -0700

    update
    update

--- src/components/header/headerViews.js
+++ src/components/header/headerViews.js
@@ -333,3 +333,11 @@ function dropDownAction() {
     x.style.display = "none";
   }
 }
+function oepnMobile() {
+  var x = document.getElementById("navbarResponsive");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
