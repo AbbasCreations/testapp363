900a5f0bdccb931284d37cdf77b656cad75faf5d
CelerantDevteam <81623214+CelerantDevteam@users.noreply.github.com>
2021-04-07 06:04:45 -0700

    uppdate
    change

--- src/components/header/headerViews.js
+++ src/components/header/headerViews.js
@@ -163,16 +163,7 @@ export const GetHeaderNavViews = () => {
   }, []);
 
 
-<script>
-function openMobile() {
-  var x = document.getElementById("navbarResponsive");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-</script>
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark static-top">
